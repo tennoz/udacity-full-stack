1d11a61503a01ac1391399280d20b1e187ce6f5f
= <=>
2017-09-21 05:09:38 +0200

    movies trailer project ready.

---
+++ movie/README.md
@@ -0,0 +1,13 @@
+#Move trailer website
+
+this application shows a list of movies and their trailers in a neat and smooth way.
+
+##How to run the application
+
+- clone the repo or download it.
+- install [python](https://www.python.org/downloads/) on your machine if not installed.
+- go to the *movies* folder in your _terminal_.
+- to run the application, type `python entertainment_center.py` in your terminal.
+
+this will open up a web page that shows you the movies list.
+
--- movie/entertainment_center.py
+++ movie/entertainment_center.py
@@ -3,33 +3,33 @@ import media
 
 slave = media.Movie("12 years a slave",
 	"n the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-	"12_Years_a_Slave_film_poster.jpg",
+	"http://cityuponahillmedia.com/blog/wp-content/uploads/2014/01/12-years-a-slave-poster.jpg",
 	"https://www.youtube.com/watch?v=z02Ie8wKKRg")
-slave = media.Movie("12 years a slave",
-	"n the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-	"12_Years_a_Slave_film_poster.jpg",
-	"https://www.youtube.com/watch?v=z02Ie8wKKRg")
+incendies = media.Movie("Incendies",
+	"Twins journey to the Middle East to discover their family history, and fulfill their mother's last wishes.",
+	"https://static1.squarespace.com/static/50d144f6e4b05aff8e5b9c8c/t/53b5dc66e4b07eed85d5b308/1404427368140/",
+	"https://www.youtube.com/watch?v=0nycksytL1A")
 
-slave = media.Movie("12 years a slave",
-	"n the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-	"12_Years_a_Slave_film_poster.jpg",
-	"https://www.youtube.com/watch?v=z02Ie8wKKRg")
+whiplash = media.Movie("whiplash",
+	"A promising young drummer enrolls at a cut-throat music conservatory where his dreams of greatness are mentored by an instructor who will stop at nothing to realize a student's potential.",
+	"http://img.moviepostershop.com/whiplash-movie-poster-2014-1010770813.jpg",
+	"https://www.youtube.com/watch?v=7d_jQycdQGo")
 
-slave = media.Movie("12 years a slave",
-	"n the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-	"12_Years_a_Slave_film_poster.jpg",
-	"https://www.youtube.com/watch?v=z02Ie8wKKRg")
+vendetta = media.Movie("V for Vendetta",
+	"In a future British tyranny, a shadowy freedom fighter, known only by the alias of \"V\", plots to overthrow it with the help of a young woman.",
+	"https://i.pinimg.com/736x/eb/2f/34/eb2f3488745a640216ce124a3c5bc65a--stephen-rea-hugo-weaving.jpg",
+	"https://www.youtube.com/watch?v=k_13fFIrhPk")
 
-slave = media.Movie("12 years a slave",
-	"n the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-	"12_Years_a_Slave_film_poster.jpg",
-	"https://www.youtube.com/watch?v=z02Ie8wKKRg")
+leon = media.Movie("Leon: The Professional",
+	"Mathilda, a 12-year-old girl, is reluctantly taken in by Leon, a professional assassin, after her family is murdered. Leon and Mathilda form an unusual relationship, as she becomes his protegee and learns the assassin's trade.",
+	"http://images4.fanpop.com/image/photos/24500000/Leon-movie-poster-leon-1994-movie-24520438-521-755.jpg",
+	"https://www.youtube.com/watch?v=DcsirofJrlM")
 
-slave = media.Movie("12 years a slave",
-	"n the antebellum United States, Solomon Northup, a free black man from upstate New York, is abducted and sold into slavery.",
-	"12_Years_a_Slave_film_poster.jpg",
-	"https://www.youtube.com/watch?v=z02Ie8wKKRg")
+city = media.Movie("City of God",
+	"Two boys growing up in a violent neighborhood of Rio de Janeiro take different paths: one becomes a photographer, the other a drug dealer.",
+	"http://media.sinematurk.com/film/7/47/e5aa4f50276f/15244_9.jpg",
+	"https://www.youtube.com/watch?v=dcUOO4Itgmw")
 
-movies = [slave, slave, slave, slave, slave, slave]
-# fresh_tomatoes.open_movies_page(movies)
-print(media.Movie.__module__)
+# create a list of movies to be passd to the open_movies_page function
+movies = [slave, incendies, whiplash, vendetta, leon, city]
+fresh_tomatoes.open_movies_page(movies)
